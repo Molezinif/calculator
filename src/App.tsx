c9480b878ad1f38f89fa7dea0dfd2f42473d8300
Molezinif <70268231+Molezinif@users.noreply.github.com>
2023-02-15 12:42:41 -0300

    firist commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+interface Operator {
+  operators: '+' | '-' | "*" | "/";
+} 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calculator">
+      <div>{/*displayValue*/}</div>
+      <button onClick={()=> {}}>7</button>
+      <button onClick={()=> {}}>8</button>
+      <button onClick={()=> {}}>9</button>
     </div>
   );
 }
